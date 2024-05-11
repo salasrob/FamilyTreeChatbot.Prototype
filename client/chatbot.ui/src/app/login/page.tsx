7d0d3ef897f8730a7ffda28afa0268d9896decd3
AdrianT <74223068+AdrTowers@users.noreply.github.com>
2024-05-11 09:11:55 -0700

    A

--- client/chatbot.ui/src/app/login/page.tsx
+++ client/chatbot.ui/src/app/login/page.tsx
@@ -1,23 +1,11 @@
 import React from 'react'
 
-export default function Login() {
+export default function LoginPage() {
   return (
     <div className='h-screen w-screen flex justify-center items-center bg-gray-700'>
-        <div className='sm:shadow-xl px-8 p-b8 pt-12 space-y-12 rounded-xl bg-gray-500'>
-            <h1 className='font-semibold text-2xl'>Log In</h1>
-        <div>
-            <label>Username</label>
-        </div>
-        <div>
-            <input className='userName' type="text" required></input>
-        </div>
-        <div>
-            <label>Password</label>
-        </div>
-        <div>
-            <input className='password' type="password" required></input>
-        </div>
-        <button>Login</button>
+        <div className='sm:shadow-xl px-8 pb-8 pt-12 space-y-12 rounded-xl bg-gray-500'>
+            <h1 className='font-semibold text-2xl text-center'>Log In</h1>
+            <p className='text-sm'>Don&apos;t have an account? Register here</p>
         </div>
     </div>
   )
